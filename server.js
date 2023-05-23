56de5b04716a27037a59ba305691445fdafed358
Mehmet <mehmet-ali-yilmaz@live.com>
2023-05-24 01:40:41 +0200

    the png is created correctly, looking for a solution to change my wallpaper now

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express()
-
 const axios = require('axios');
+const fs = require("fs");
+const { createCanvas } = require('canvas');
 
 app.use(express.json());
 
@@ -24,13 +25,36 @@ function processContributions(events) {
     return contributions;
 }
 
-const { createCanvas } = require('canvas');
-
 function createImage(contributions, width, height) {
     const canvas = createCanvas(width, height);
     const context = canvas.getContext('2d');
 
-    // TODO: Draw contributions graph based on 'contributions' data
+    const squareSize = Math.floor(width / 7); // 7 days in a week
+
+    // Draw contributions graph based on 'contributions' data
+    let x = 0, y = 0;
+    for (let date in contributions) {
+        // Choose color based on number of contributions
+        let color;
+        switch (contributions[date]) {
+            case 0: color = '#ebedf0'; break; // no contributions: light gray
+            case 1: color = '#9be9a8'; break; // 1 contribution: light green
+            case 2: color = '#40c463'; break; // 2 contributions: green
+            case 3: color = '#30a14e'; break; // 3 contributions: dark green
+            default: color = '#216e39'; break; // 4 or more contributions: darkest green
+        }
+
+        // Draw square
+        context.fillStyle = color;
+        context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
+
+        // Move to next position in grid
+        x++;
+        if (x >= 7) {
+            x = 0;
+            y++;
+        }
+    }
 
     const out = fs.createWriteStream(__dirname + '/contributions.png');
     const stream = canvas.createPNGStream();
@@ -38,11 +62,7 @@ function createImage(contributions, width, height) {
     out.on('finish', () => console.log('The PNG file was created.'));
 }
 
-// const wallpaper = require('wallpaper');
 
-// async function setImageAsWallpaper() {
-//     await wallpaper.set('./contributions.png');
-// }
 let wallpaper;
 import('wallpaper').then((module) => {
     wallpaper = module;
@@ -51,7 +71,7 @@ import('wallpaper').then((module) => {
 
 app.get("/", async (req, res) => {
     const fetch = await fetchUserActivity("Daar93");
-    console.log(fetch)
+    createImage(processContributions(fetch), 1920, 1080);
 
     res.send("Hallo");
 })
